151d268482baa38e23a1296655243ebe49cea7d3
Ranaivoson <air_manitra@yahoo.fr>
2020-07-31 12:27:48 -0400

    Ajoutez des fichiers projet.

---
+++ code-katas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace code_katas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DigitalRoot(456));
+        }
+
+        static int DigitalRoot(long n)
+        {
+            var arrayOfDigitCharacter = n.ToString().ToCharArray();
+            int total = 0;
+            foreach (char element in arrayOfDigitCharacter)
+            {
+                total += int.Parse(element.ToString());
+            }
+            if (total > 9) {
+                return DigitalRoot(total);
+            } else {
+                return total;
+            }
+        }
+    }
+}
