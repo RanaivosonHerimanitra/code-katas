c0cf015a6a95aef4688a197bdf504975796d13a5
Ranaivoson <air_manitra@yahoo.fr>
2020-08-02 19:28:28 -0400

    quickUnion

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # code-katas
 My code katas
+
+* Erastothene crible to find prime numbers under `N`.
+
+* Non weighted quick union. This algorithm is useful to determine if any given 02 elements are linked together. The data representation is a tree structure, implemented by array.
+
---
+++ code-katas/QuickUnion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace code_katas
+{
+    class QuickUnion
+    {
+        private int[] instanceArray;
+
+        public QuickUnion(int N)
+        {
+            instanceArray = new int[N];
+            for (int i = 0; i < N; i++) instanceArray[i] = i;
+        }
+
+        private int Root(int i)
+        {
+            while (i != instanceArray[i]) i = instanceArray[i];
+            return i;
+        }
+
+        public bool Connected(int p, int q)
+        {
+            return Root(p) == Root(q);
+        }
+
+        public void Union(int p, int q)
+        {
+            int i = Root(p);
+            int j = Root(q);
+            instanceArray[i] = j;
+        }
+    }
+}
